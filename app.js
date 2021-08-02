e6a5b884a9a9f923bd25445b45b046cb128543d6
ssyyhhrr <44934807+ssyyhhrr@users.noreply.github.com>
2021-08-02 23:45:06 +0100

    Frontend

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ app.get('/', async function(req, res) {
     console.log(`[${timestamp()}] [${req.ip}] [${req.method}] ${req.protocol} ${req.originalUrl}`)
     return res.render('index')
 })
+
+app.listen(3000, () => {
+    console.log(`[${timestamp()}] [START] app.js`)
+})
+
+function timestamp() {
+    let date = new Date()
+
+    let day = ("0" + date.getDate()).slice(-2)
+    let month = ("0" + (date.getMonth() + 1)).slice(-2)
+    let year = date.getFullYear()
+
+    let hours = ("0" + (date.getHours())).slice(-2)
+    let minutes = ("0" + (date.getMinutes())).slice(-2)
+    let seconds = ("0" + (date.getSeconds())).slice(-2)
+
+    return (day + "/" + month + "/" + year + " " + hours + ":" + minutes + ":" + seconds)
+}
